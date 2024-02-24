4f4bb73b4ad5f13662519ae6fe62b9e7efb2d631
MarianaD167 <dyshkantt228@gmail.com>
2024-02-24 16:48:10 +0200

    PR_1

--- Solution1/pr1/pr1.cpp
+++ Solution1/pr1/pr1.cpp
@@ -2,10 +2,144 @@
 //
 
 #include <iostream>
+#include <string>
+#include <locale>
+using namespace std;
+
+
+void z1() {
+
+	int n1 = 1;
+	int n2 = 2;
+	int n3 = 3;
+
+	if (1<=n1 && n1<=6 ) {
+		cout << n1 << " ";
+	}
+
+	if (1 <= n2 && n2 <= 6) {
+		cout << n2 << " ";
+	}
+
+	if (1 <= n3 && n3 <= 6) {
+		cout << n3;
+	}
+
+}
+
+void z2() {
+
+	int n1 = 1;
+	int n2 = 2;
+	int n3 = 3;
+
+	if (n1 < n2 && n1 < n3) {
+		cout << n1;
+	}
+
+	if (n2 < n1 && n2 < n3) {
+		cout << n2;
+	}
+
+	if (n3 < n1 && n3 < n2) {
+		cout << n3;
+	}
+}
+
+
+void z3 (){
+
+	int num;
+	cout << "Введiть кiлькiсть: ";
+	cin >> num;
+
+	double sum = 0;
+	double n = 1;
+
+	for (int i = 0; i < num; ++i) {
+		cout << n;
+		sum += n;
+		n /= 2;
+	}
+
+	cout << endl;
+	cout << "Сума:" << sum;
+
+
+}
+
+void z4() {
+	 
+	int n = 8;
+	int v = 2;
+
+	while (n % v != 0) {
+		++v;
+	}
+	cout << "Найменший дiльник:" << v;
+	
+
+}
+
+
+void z5() {
+
+	int n;
+	cout << "Введiть кiлькiсть елементiв послдовност Фiбоначчi: ";
+	cin >> n;
+
+	int a = 0;
+	int	b = 1;
+	int nextTerm;
+
+	cout << "Послiдовнiсть Фiбоначчi: ";
+
+	for (int i = 1; i <= n; ++i) {
+		
+		cout << a << " ";
+		nextTerm = a + b;
+		a = b;
+		b = nextTerm;
+	}
+	cout << endl;
+
+}
+
+void z6() {
+
+	srand(time(0));
+	int a = 1 + rand() % 20;
+
+	for (int i = 1;true;++i) {
+	
+    int num;
+	cout << "Введiть число вiд 1 - 20:";
+	cin >> num;
+
+	if (num > a) {
+		cout << "Запропоноване число менше задуманого";
+	}
+
+	if (num < a) {
+		cout << "Запропоноване число бiльше задуманого";
+	}
+
+	if (num == a) {
+		cout << "Ви вгадали число з "<< i <<"разу";
+		break;
+	}
+
+	}
+
+	
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_CTYPE, "ukr");
+	z6();
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
